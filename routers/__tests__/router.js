2bf5a8fca34b36d10910c1e0facd02d12a7e8c9c
Brian G <brian.gregorius@gmail.com>
2019-02-16 18:22:37 -0500

    added delete test

--- routers/__tests__/router.js
+++ routers/__tests__/router.js
@@ -9,20 +9,27 @@ describe("router.js", () => {
       expect(response.status).toBe(501);
     });
   });
-});
 
-describe("get /job/:id route", () => {
-  it("returns status code 500", async () => {
-    let response = await request(server).get("/job/:id");
-    expect(response.status).toBe(200);
+  describe("get /job/:id route", () => {
+    it("returns status code 500", async () => {
+      let response = await request(server).get("/job/:id");
+      expect(response.status).toBe(200);
+    });
   });
-});
 
-describe("post /job route", () => {
-  it("returns status code 500", async () => {
-    let response = await request(server)
-      .post("/")
-      .send();
-    expect(response.status).toBe(500);
+  describe("post /job route", () => {
+    it("returns status code 500", async () => {
+      let response = await request(server)
+        .post("/")
+        .send();
+      expect(response.status).toBe(500);
+    });
+  });
+
+  describe("delete /job:/id route", () => {
+    it("returns status code 500", async () => {
+      let response = await request(server).delete("/:id");
+      expect(response.status).toBe(500);
+    });
   });
 });
