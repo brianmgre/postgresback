2c4a182b0bbf320dff3c94ecb9e3541d977dd748
Brian G <brian.gregorius@gmail.com>
2019-02-16 18:21:05 -0500

    test for get single job

--- routers/__tests__/router.js
+++ routers/__tests__/router.js
@@ -3,9 +3,17 @@ const request = require("supertest");
 const server = require("../../api/server.js");
 
 describe("router.js", () => {
-    describe("get /job route", () => {
-        it("returns status code 501", async () => {
-            let response = await request(server).get("/job");
-            expect(response.status).toBe(501);
-        });
+  describe("get /job route", () => {
+    it("returns status code 501", async () => {
+      let response = await request(server).get("/job");
+      expect(response.status).toBe(501);
     });
+  });
+});
+
+describe("get /job/:id route", () => {
+  it("returns status code 500", async () => {
+    let response = await request(server).get("/job/:id");
+    expect(response.status).toBe(200);
+  });
+});
